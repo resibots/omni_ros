36952d225549efe63dcbf076b9d8cb38eafcf9f0
Dorian Goepp <dorian.goepp@inria.fr>
2018-05-17 17:19:13 +0200

    Fix the NoSafetyConstraint class

--- omni_controllers/include/omni_controllers/arm_speed_safe_controller.hpp
+++ omni_controllers/include/omni_controllers/arm_speed_safe_controller.hpp
@@ -40,14 +40,14 @@
 #ifndef OMNI_CONTROLLERS_ARM_SPEED_SAFE_H
 #define OMNI_CONTROLLERS_ARM_SPEED_SAFE_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
-#include <ros/node_handle.h>
-#include <hardware_interface/joint_command_interface.h>
 #include <controller_interface/controller.h>
-#include <std_msgs/Float64MultiArray.h>
+#include <hardware_interface/joint_command_interface.h>
 #include <realtime_tools/realtime_buffer.h>
+#include <ros/node_handle.h>
+#include <std_msgs/Float64MultiArray.h>
 
 namespace arm_speed_safe_controller {
 
@@ -152,8 +152,14 @@ namespace arm_speed_safe_controller {
     /** \cond HIDDEN_SYMBOLS */
     struct NoSafetyConstraints {
         bool init(const std::vector<std::shared_ptr<hardware_interface::JointHandle>>& joints,
-            ros::NodeHandle& nh);
-        bool enforce(std::vector<double>& commands, const ros::Duration& period);
+            ros::NodeHandle& nh)
+        {
+            return true;
+        }
+        bool enforce(std::vector<double>& commands, const ros::Duration& period)
+        {
+            return true;
+        }
     };
     /** \endcond */
 
