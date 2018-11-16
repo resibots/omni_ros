c8e0713ee0780ad4dfc7282ad870880799e13157
Debaleena <debaleena.misra@gmail.com>
2018-11-16 14:02:11 +0100

    wip: rates are a problem but works

--- omni_controllers/include/omni_controllers/policy_controller_with_reset.hpp
+++ omni_controllers/include/omni_controllers/policy_controller_with_reset.hpp
@@ -170,7 +170,7 @@ namespace arm_speed_safe_controller {
           if (_mpc_flag) // Blackdrops parameters to be implemented
             {
                // ROS_INFO("Inside UPDATE : Starting mpc flag=true related actions");
-               if (_episode_iterations < 40) //During the episode (here it is set to only one step episodes), when mpc commands can be sent
+               if (_episode_iterations < 5) //During the episode (here it is set to only one step episodes), when mpc commands can be sent
                 {
                     _commands = Eigen::VectorXd::Map(_mpc_commands.data(), _mpc_commands.size());
                     ROS_INFO("Executing action for one step..");
@@ -294,31 +294,31 @@ namespace arm_speed_safe_controller {
                     _jointList.clear();
                 }
 
-                if (_realtime_pub_commands->trylock()) {
-
-                    _realtime_pub_commands->msg_.val.clear();
-                    // ROS_INFO("cleared the message in realtime command pub");
-                    double tmpval;
-                    for(int i=0; i< _commandList.size(); i++){
-                        tmpval = _commandList[i];
-                       _realtime_pub_commands->msg_.val.push_back(tmpval);
-                    }
-
-                    _realtime_pub_commands->unlockAndPublish();
-
-                    if (_realtime_pub_commands->trylock()) {
-                      _realtime_pub_commands->msg_.val.clear();
-                      // ROS_INFO("cleared the message in realtime command pub");
-                      _realtime_pub_commands->unlock();
-                    }
-
-                  _commandList.clear();
-                }
-
-                if (_realtime_pub_margin->trylock()) {
-                    _realtime_pub_margin->msg_.data = _constraint.consult(period);
-                    _realtime_pub_margin->unlockAndPublish();
-                }
+                // if (_realtime_pub_commands->trylock()) {
+                //
+                //     _realtime_pub_commands->msg_.val.clear();
+                //     // ROS_INFO("cleared the message in realtime command pub");
+                //     double tmpval;
+                //     for(int i=0; i< _commandList.size(); i++){
+                //         tmpval = _commandList[i];
+                //        _realtime_pub_commands->msg_.val.push_back(tmpval);
+                //     }
+                //
+                //     _realtime_pub_commands->unlockAndPublish();
+                //
+                //     if (_realtime_pub_commands->trylock()) {
+                //       _realtime_pub_commands->msg_.val.clear();
+                //       // ROS_INFO("cleared the message in realtime command pub");
+                //       _realtime_pub_commands->unlock();
+                //     }
+                //
+                //   _commandList.clear();
+                // }
+                //
+                // if (_realtime_pub_margin->trylock()) {
+                //     _realtime_pub_margin->msg_.data = _constraint.consult(period);
+                //     _realtime_pub_margin->unlockAndPublish();
+                // }
 
                 publish_flag = false;
             } //end of publishing
@@ -360,14 +360,11 @@ namespace arm_speed_safe_controller {
           //Maybe have to clear _mpc_commands first
           _mpc_commands.clear();
           _mpc_flag = true;
-          publish_flag = false;
+          // publish_flag = false;
           _episode_iterations = 1;
           double tmpval;
           for (int i = 0; i < msg->val.size(); i++) {
               tmpval = msg->val[i];
-              // ROS_INFO("received value:", tmpval);
-              // std::cout <<"received command:"<< tmpval << std::endl;
-              //_mpc_commands[i] = tmpval;
               _mpc_commands.push_back(tmpval);
             }
           ROS_INFO("Commands received!");
@@ -380,16 +377,7 @@ namespace arm_speed_safe_controller {
             return true;
         }
 
-        // inline Eigen::VectorXd states_to_eigen()
-        // {
-        //     Eigen::VectorXd res(joints.size());
-        //
-        //     for (size_t i = 0; i < joints.size(); ++i) //Arm
-        //         res[i] = joints[i]->getPosition();
-        //
-        //     return res;
-        // }
-    }; // policy_controller
+  }; // policy_controller
 } // namespace arm_speed_safe_controller
 
 #endif
